4f6e1fc7fcc532e70074c1dacf104845b0ade32c
User <User@DESKTOP-9TGTEUB>
2023-11-28 03:03:41 +0400

    Add project files.

---
+++ ConsoleApplication17/ConsoleApplication17.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+	unsigned int a;
+	std::cin >> a;
+	unsigned int v = a * a * a;
+	unsigned s = 6 * a * a;
+	std::cout << v << std::endl;
+	std::cout << s << std::endl;
+	return 0;
+}
